cc1a56a97b12954c926f5b73645140e1f49a3828
Joel Taylor <joel.w.taylor@gmail.com>
2026-06-07 13:41:07 +0100

    Initial Commit

---
+++ src/client/java/com/joel4848/extraplaceholders/client/ExtraPlaceholdersClient.java
@@ -0,0 +1,10 @@
+package com.joel4848.extraplaceholders.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraPlaceholdersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
